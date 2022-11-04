40b2f72cb11922905e673bf6b9d2808eabde946e
peevs99 <88054944+peevs99@users.noreply.github.com>
2022-11-04 19:53:46 -0400

    Update Memory.java

--- Memory.java
+++ Memory.java
@@ -1,9 +1,21 @@
 import java.util.Arrays;
 import java.util.List;
 
+// /**
+// Memory Control Unit
+// Reserved Locations Of Memory:
+// 0 - Reserved for the Trap instruction for Part III.
+// 1 - Reserved for a machine fault
+// 2 - Store PC for Trap
+// 3 - NU
+// 4 - Store PC for Machine Fault
+// 5 - NU
+
 public class Memory
 {
 
+    // memory size 2048 initially
+    // expandable memory size to 4096
     static int size = 2048;
     static int[] ProcessorMemory = new int[size];
     static int[] ProcessorMemoryTemp;
@@ -45,21 +57,25 @@ public class Memory
 
     public static int get_from_memory_int(int addr)
     {
+        // Fetching memory as integer
         return ProcessorMemory[addr];
     }
 
     public static String get_from_memory_string(int addr)
     {
+        // Fetrching memory as string
         return String.format("%16s", Integer.toBinaryString(ProcessorMemory[addr])).replace(' ', '0');
     }
 
     public static void store_to_memory(int addr, int val)
     {
+        //  Store directly into memory using address and value
         ProcessorMemory[addr] = val;
     }
 
     public static void clear_memory_address(int addr)
     {
+        // Clearing Memory address
         ProcessorMemory[addr] = 0;
     }
 
